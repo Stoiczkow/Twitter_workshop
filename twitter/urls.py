a29c7f65a0a6b25a7dc7215b3096b74b51f26698
Stoiczkow <pawelwyzykowski@gmail.com>
2018-08-01 11:40:27 +0200

    fixed login and logout

--- twitter/urls.py
+++ twitter/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPageView.as_view()),
-    path('login/', auth_views.login, name='login')
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, {'next_page': '/login/'}, name='logout'),
 ]
