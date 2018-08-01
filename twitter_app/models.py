f24c1623ca9d2afa9c6829d6b3f10de8c8c08480
Stoiczkow <pawelwyzykowski@gmail.com>
2018-08-01 09:07:58 +0200

    added models

--- twitter_app/models.py
+++ twitter_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Tweet(models.Model):
+    text = models.TextField(max_length=140)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=True)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=140)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=True)
+    tweet = models.ForeignKey(Tweet, on_delete=True)
