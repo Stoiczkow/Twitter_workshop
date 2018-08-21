cd1fcf638dcf0adb53c395e2a293b93387cf0b27
Stoiczkow <pawelwyzykowski@gmail.com>
2018-08-21 10:30:37 +0200

    fixed problem with anon user

--- twitter_app/cp.py
+++ twitter_app/cp.py
@@ -1,9 +1,13 @@
 from .models import PrivateMessage
 
 def unread_msgs(request):
-    unread_msgs = PrivateMessage.objects.filter(recipient=request.user,
-                                                is_read=False).count()
-    return {
-        "unread_msgs" : unread_msgs
+    if request.user.id is not None:
+        unread_msgs = PrivateMessage.objects.filter(recipient=request.user,
+                                                    is_read=False).count()
+        return {
+            "unread_msgs" : unread_msgs
+            }
+    else:
+        return {
+            "unread_msgs": 0
         }
-
